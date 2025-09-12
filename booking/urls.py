9e40e21f795441688f55a27ae6dbcffd84c96bae
Teepop Bambudpai <6610685197@cn330>
2025-09-12 19:07:11 +0700

    modified booking/urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -6,5 +6,4 @@ app_name = "booking"
 urlpatterns = [
     path("", views.index, name="index"),
     path("my-bookings/", views.my_bookings, name="my-bookings"),
-    path("cancel/<int:booking_id>/", views.cancel_booking, name="cancel_booking"),
 ]
