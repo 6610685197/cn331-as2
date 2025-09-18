eb1a50b2ff74ac6d61139fcca3986daec803ee8f
Teepop Bambudpai <6610685197@cn330>
2025-09-18 21:34:57 +0700

    edit build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --email "your@email.com" --
-	noinput || true
+python manage.py createsuperuser --username admin --email "your@email.com" --noinput || true
