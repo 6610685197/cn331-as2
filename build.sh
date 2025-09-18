907a41bab43f9b515d0551a8747604a55dc83378
Teepop Bambudpai <6610685197@cn330>
2025-09-18 21:00:48 +0700

    edit build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # Exit on error
 
 set-o errexit
-pip install--upgrade pip
-pip install-r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 python manage.py collectstatic--no-input
 python manage.py migrate
 python manage.py createsuperuser--username admin--email "your@email.com"-
