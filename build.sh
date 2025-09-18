c27b1f2a9fe8e204fd50d4378eee9637b4aa8518
Teepop Bambudpai <6610685197@cn330>
2025-09-18 22:04:21 +0700

    edit build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_USERNAME=admin1 \
 DJANGO_SUPERUSER_EMAIL=admin@email.com \
 DJANGO_SUPERUSER_PASSWORD=1234 \
 python manage.py createsuperuser --noinput || true
